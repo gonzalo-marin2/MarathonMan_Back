d654069148585eae5741e89dd7b536e136fac30d
Sofia Martin Román <s.martinroma93@gmail.com>
2020-12-09 12:09:36 +0100

    eliminacion usuarios.js y usuario.js

--- models/corredor.js
+++ models/corredor.js
@@ -1,6 +1,6 @@
 const getAll = () => {
     return new Promise((resolve, reject) => {
-        db.query('select * from usuarios', (error, rows) => {
+        db.query('select * from corredores', (error, rows) => {
             if (error) reject(error);
             resolve(rows)
         });
@@ -8,20 +8,20 @@ const getAll = () => {
 };
 
 
-const create = ({ nombre, apellidos, edad, email, telefono, nivel, zona }) => {
+/* const create = ({ nombre, apellidos, edad, email, telefono, nivel, zona }) => {
     return new Promise((resolve, reject) => {
-        db.query('INSERT INTO usuarios (nombre, apellidos, edad, email, telefono, nivel, zona) values (?, ?, ?, ?, ?, ?, ?)', [nombre, apellidos, edad, email, telefono, nivel, zona], (error, result) => {
+        db.query('INSERT INTO corredores (nombre, apellidos, edad, email, telefono, nivel, zona) values (?, ?, ?, ?, ?, ?, ?)', [nombre, apellidos, edad, email, telefono, nivel, zona], (error, result) => {
             if (error) reject(error);
             resolve(result);
         }
         )
     })
-}
+} */
 
 
 const getById = (pUsuarioId) => {
     return new Promise((resolve, reject) => {
-        db.query('select * from usuarios where id=?', [pUsuarioId], (error, rows) => {
+        db.query('select * from corredores where id=?', [pUsuarioId], (error, rows) => {
             if (error) reject(error);
             if (rows.length === 0) resolve(null);
             resolve(rows[0]);
@@ -30,9 +30,9 @@ const getById = (pUsuarioId) => {
 }
 
 
-const deleteById = (pUsuarioId) => {
+/* const deleteById = (pUsuarioId) => {
     return new Promise((resolve, reject) => {
-        db.query('delete from clientes where id = ?', [pUsuarioId], (error, result) => {
+        db.query('delete from corredores where id = ?', [pUsuarioId], (error, result) => {
             if (error) reject(error)
             resolve(result);
         })
@@ -42,13 +42,15 @@ const deleteById = (pUsuarioId) => {
 
 const updateById = (pUsuarioId, { nombre, apellidos, edad, email, telefono, nivel, zona }) => {
     return new Promise((resolve, reject) => {
-        db.query('update usuarios set nombre = ?, apellidos = ?, edad = ?, email = ?, telefono = ?, nivel = ?, zona = ?', [nombre, apellidos, edad, email, telefono, nivel, zona, pUsuarioId]), (error, result) => {
+        db.query('update corredores set nombre = ?, apellidos = ?, edad = ?, email = ?, telefono = ?, nivel = ?, zona = ?', [nombre, apellidos, edad, email, telefono, nivel, zona, pUsuarioId]), (error, result) => {
             if (error) reject(error);
             resolve(result);
         }
     })
-}
+} */
 
+
+const getByEvento
 
 module.exports = {
     getAll, create, getById, deleteById, updateById
