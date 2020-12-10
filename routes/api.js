f33570bc33c3b8d9cacbd6103505f38e70e32980
Sofia Martin Román <s.martinroma93@gmail.com>
2020-12-10 16:11:43 +0100

    router comentarios

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const apiEventosRouter = require('./api/eventos');
 const apiCorredoresRouter = require('./api/corredores');
+const apiComentariosRouter = require('./api/comentarios')
 
 router.use('/eventos', apiEventosRouter);
 router.use('/corredores', apiCorredoresRouter);
+router.use('/comentarios', apiComentariosRouter);
 
 
 module.exports = router;
--- routes/api/comentarios.js
+++ routes/api/comentarios.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { getAll, create } = require('../../models/comentario')
+const { getAll, create } = require('../../models/comentario');
 
 
 router.get('/', async (req, res) => {
@@ -10,6 +10,24 @@ router.get('/', async (req, res) => {
         res.json({ error: error.message })
     }
 });
+
+
+router.post('/', async (req, res) => {
+    try {
+        const result = await create(req.body);
+        if (result.affectedRows === 1) {
+            const nuevoEvento = await getById(result.insertId);
+            res.json({
+                mensaje: 'Se ha insertado correctamente',
+                evento: nuevoEvento
+            });
+        } else {
+            res.json({ error: 'No se ha podido insertar' })
+        }
+    } catch (error) {
+        res.json({ error: error.message });
+    }
+})
 
 
 module.exports = router;
