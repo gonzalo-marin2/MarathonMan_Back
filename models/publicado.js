53f202aaca9967215ff322f0a77ab47a16c7f740
gonzalo-marin <gonzalo.marn@gmail.com>
2020-12-21 17:40:35 +0100

    peticiones.rest y trabajo sobre publicado

--- models/publicado.js
+++ models/publicado.js
@@ -7,10 +7,10 @@ const getAll = () => {
     });
 };
 
-const create = (pObjeto) => {
+const create = (pObjeto, pFk_corredor) => {
     console.log(pObjeto);
     return new Promise((resolve, reject) => {
-        db.query('INSERT INTO publicaciones (titulo, comentario) values (?, ?)', [pObjeto.titulo, pObjeto.comentario], (error, result) => {
+        db.query('INSERT INTO publicaciones (titulo, comentario, fk_corredor) values (?, ?, ?)', [pObjeto.titulo, pObjeto.comentario, pFk_corredor], (error, result) => {
             if (error) reject(error);
             resolve(result);
         }
@@ -29,6 +29,16 @@ const getById = (pComentarioId) => {
     })
 }
 
+const getByCorredorId = (pCorredorId) => {
+    return new Promise((resolve, reject) => {
+        db.query('select * from publicaciones where fk_corredor = ?', [pCorredorId], (error, rows) => {
+            if (error) reject(error);
+            if (rows.length === 0) resolve(null);
+            resolve(rows);
+        })
+    })
+}
+
 const deleteById = (pComentarioId) => {
     return new Promise((resolve, reject) => {
         db.query('delete from publicaciones where id = ?', [pComentarioId], (error, result) => {
@@ -38,4 +48,4 @@ const deleteById = (pComentarioId) => {
     });
 }
 
-module.exports = { getAll, create, getById, deleteById }
+module.exports = { getAll, create, getById, deleteById, getByCorredorId }
--- routes/api/publicados.js
+++ routes/api/publicados.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const { getAll, create, getById, deleteById } = require('../../models/publicado');
+const { getAll, create, getById, deleteById, getByCorredorId } = require('../../models/publicado');
+const { checkToken } = require('../middlewares');
 
 router.get('/', async (req, res) => {
     try {
@@ -9,5 +10,32 @@ router.get('/', async (req, res) => {
         res.json({ error: error.message })
     }
 });
+
+router.get('/:corredorId', checkToken, async (req, res) => {
+    try {
+        const rows = await getByCorredorId(req.user.id);
+        res.json(rows);
+    } catch (error) {
+        res.json({ error: error.message })
+    }
+});
+
+router.post('/', checkToken, async (req, res) => {
+    console.log(req.body);
+    try {
+        const result = await create(req.body, req.user.id);
+        if (result.affectedRows === 1) {
+            const nuevoPublicado = await getById(result.insertId);
+            res.json({
+                mensaje: 'Se ha insertado correctamente',
+                comentario: nuevoPublicado
+            });
+        } else {
+            res.json({ error: 'No se ha podido insertar' })
+        }
+    } catch (error) {
+        res.json({ error: error.message });
+    }
+})
 
 module.exports = router;
